12e19ee0ab4d4f54e52d199e591ac9a7a9ba9fc0
lizathrc <106450334+lizathrc@users.noreply.github.com>
2022-08-21 15:35:54 -0500

    Delete sem10.js

--- practicasem10/sem10.js
+++
@@ -1,9 +0,0 @@
-let list = parseInt(process.argv.slice(2));
-let ciclo = 1;
-
-for (let i = 0; i < list.length; i++) {
-  if (process.argv.slice(2) [i] != ',') {
-    console.log(list [0]);
-    ciclo = parseInt(list [i]) * ciclo;
-  }
-} console.log(ciclo);
